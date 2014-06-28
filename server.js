0ed9c28706cbb5160c7aeae9fbafc297fa89f5b1
adhulappanavar <anil.dhulappanavar@gmail.com>
2014-06-28 21:50:11 +0530

    Adding Express Webserver And Server Basic index.html
    Adding Express Webserver And Server Basic index.html

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-console.log("hello!")
+var express = require('express')
+  , http = require('http')
+  , path = require('path')
+
+var app = express()
+
+var clientDir = path.join(__dirname, 'client')
+
+app.configure(function() {
+  app.set('port', process.env.PORT || 3000)
+  app.use(express.logger('dev'))
+  app.use(express.bodyParser()) //parses json, multi-part (file), url-encoded
+  app.use(app.router) //need to be explicit, (automatically adds it if you forget)
+  app.use(express.static(clientDir)) //should cache static assets
+})
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+})
+
+app.get('/', function(req, res) {
+  res.sendfile(path.join(clientDir, 'index.html'))
+})
